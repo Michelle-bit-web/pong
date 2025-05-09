411f016f3f5947350400353b3d08eac41a5bd2a4
Michelle <mpuschkarow@web.de>
2025-05-09 14:39:25 +0200

    interval added that changes ball position to keep it moving

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let ball = {
     x: 360,
     y: 240,
     speedX: 3,
-    speedY: 0,
+    speedY: 1,
 };
 
 document.addEventListener('keydown', event => key[event.keyCode] = true);
@@ -17,14 +17,24 @@ function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     draw();
+    let loopInterval = setInterval(() => loop(), 1000 / 60)
 }
 
 function draw(){
-ctx.fillStyle = 'white'; //weiße Elemente
+ctx.clearRect(0, 0, canvas.width, canvas.height);  //initial clearance of canvas  
+
+ctx.fillStyle = 'white'; //white elements
 ctx.fillRect(700, p1, 10, 80);
 ctx.fillRect(10, p2, 10, 80);
+
+ctx.beginPath(); //start balöl
 ctx.arc(ball.x, ball.y, 5, 0, Math.PI * 2); //x, y, radius, angle or start, 360 degree 
 ctx.fill();    
 
 requestAnimationFrame(draw); // for repetitive drawing
+}
+
+function loop(){
+    ball.x += ball.speedX; //change x direction
+    ball.y += ball.speedY; //change y direction
 }
