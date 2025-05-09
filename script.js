afc07d0e6be41abdef18f6b93de1445177667bdc
Michelle <mpuschkarow@web.de>
2025-05-09 14:22:06 +0200

    key event listener

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let canvas;
 let ctx;
+let p1 = 80; //initial position y player 1
+let p2 = 80; //player 2
+let key = {}; //for pressed keys
+let ball = {
+    x: 360,
+    y: 240,
+    speedX: 3,
+    speedY: 0,
+};
+
+document.addEventListener('keydown', event => key[event.keyCode] = true);
+document.addEventListener('keyup', event => key[event.keyCode] = false);
 
 function init(){
     canvas = document.getElementById('canvas');
@@ -9,8 +21,10 @@ function init(){
 
 function draw(){
 ctx.fillStyle = 'white'; //weiße Elemente
-ctx.fillRect(700, 10, 10, 80);
-ctx.fillRect(10, 10, 10, 80);
-ctx.arc(350, 100, 5, 0, Math.PI * 2);
+ctx.fillRect(700, p1, 10, 80);
+ctx.fillRect(10, p2, 10, 80);
+ctx.arc(ball.x, ball.y, 5, 0, Math.PI * 2); //x, y, radius, angle or start, 360 degree 
 ctx.fill();    
+
+requestAnimationFrame(draw); // for repetitive drawing
 }
